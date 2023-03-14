f20721779affa5157cd0f08b1cab8238019ecec7
MahmoudKSultan <mahmoodkns@hotmail.com>
2023-03-14 18:44:07 +0200

    UI

--- src/features/payout/withdraw/components/Cash/index.tsx
+++ src/features/payout/withdraw/components/Cash/index.tsx
@@ -1,210 +1,273 @@
-import { Button, IconButton, Input } from "components";
-import { useState } from "react";
-import { useForm } from 'react-hook-form';
+import { Fragment, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import { ArrowDownIcon, PlusIcon } from "lib/@heroicons";
+import { Divider } from "components";
+import Option from "./../Option";
+import SelectList from "../SelectList";
 import useModal from "hooks/useModal";
 import Modal from "components/Modal";
-import { DeleteIcon, EditIcon } from "lib/@heroicons";
-import { Dialog } from "@headlessui/react";
-import { MiniCard, TransferCard, ModalAddRecipient, SelectListBox } from 'features/payout';
+import AddRecipient from "../AddRecipient";
+import { TransferCard } from "features/payout";
+import EditRecipient from "../EditRecipient";
 
-type FormValues = {
-    amount: string;
-    // select: object;
+const offices = [
+  {
+    _id: "6310a930d74275d6cdd15be3",
+    name: "مكتب الدانا",
+    address: "غزة-شارع الشهداء، مقابل برج فلسطين",
+    fees: 1,
+    startingHour: "08:00",
+    endingHour: "20:00",
+  },
+  {
+    _id: "6342c8cb413b5bf1722e33f5",
+    name: "مكتب حرزالله",
+    address: "غزة-شارع الشهداء، مقابل برج فلسطين",
+    fees: 1,
+    startingHour: "08:00",
+    endingHour: "20:00",
+  },
+  {
+    _id: "637373ba54ceb455af5a2751",
+    name: "مكتب الدانا",
+    address: "غزة-شارع الشهداء، مقابل برج فلسطين",
+    fees: 1,
+    startingHour: "08:00",
+    endingHour: "10:00",
+  },
+  {
+    _id: "6373741e28859db2b7c1d697",
+    name: "مكتب الدانا",
+    address: "غزة-شارع الشهداء، مقابل برج فلسطين",
+    fees: 1,
+    startingHour: "08:00",
+    endingHour: "10:00",
+  },
+  {
+    _id: "6373796e48487798d2ae3913",
+    name: "مكتب الدانا",
+    address: "غزة-شارع الشهداء، مقابل برج فلسطين",
+    fees: 0,
+    startingHour: "08:00",
+    endingHour: "10:00",
+  },
+  {
+    _id: "6373799448487798d2ae3916",
+    name: "مكتب الدانا",
+    address: "غزة-شارع الشهداء، مقابل برج فلسطين",
+    fees: 0,
+    startingHour: "08:00",
+    endingHour: "10:00",
+  },
+  {
+    _id: "637379c1b0e12ea57c3ac2ce",
+    name: "مكتب الدانا",
+    address: "غزة-شارع الشهداء، مقابل برج فلسطين",
+    fees: 0,
+    startingHour: "08:00",
+    endingHour: "10:00",
+  },
+  {
+    _id: "637379f0b0e12ea57c3ac2d1",
+    name: "مكتب الدانا",
+    address: "غزة-شارع الشهداء، مقابل برج فلسطين",
+    fees: 1,
+    startingHour: "08:00",
+    endingHour: "10:59",
+  },
+  {
+    _id: "63b449d5efdc62544da3cb40",
+    name: "مكتب الدانا",
+    address: "غزة-شارع الشهداء، مقابل برج فلسطين",
+    fees: 1,
+    startingHour: "08:00",
+    endingHour: "15:59",
+  },
+  {
+    _id: "63b6f40f4e18dcc1124db800",
+    name: "مكتب الدانا",
+    address: "غزة-شارع الشهداء، مقابل برج فلسطين",
+    fees: 1,
+    startingHour: "08:00",
+    endingHour: "15:59",
+  },
+];
+
+const recipients = [
+  {
+    _id: "641037e62072538a20bedab5",
+    name: "Ahmed Thabet",
+    mobile: "+970597039224",
+    idNumber: "123456788",
+    createdAt: "2023-03-14T09:01:26.252Z",
+    updatedAt: "2023-03-14T09:01:26.252Z",
+  },
+  {
+    _id: "640f9158a3350410c1a420d2",
+    name: "Heba Skhail",
+    mobile: "+970597039225",
+    idNumber: "123456709",
+    createdAt: "2023-03-13T21:10:48.320Z",
+    updatedAt: "2023-03-13T21:10:48.320Z",
+  },
+  {
+    _id: "640f5ec5a6e6654e4fc9a769",
+    name: "Heba Skhail",
+    mobile: "+970597039225",
+    idNumber: "1234567090",
+    createdAt: "2023-03-13T17:35:01.763Z",
+    updatedAt: "2023-03-13T17:35:01.763Z",
+  },
+  {
+    _id: "640f5eb4a6e6654e4fc9a757",
+    name: "Heba Skhail",
+    mobile: "+970597039225",
+    idNumber: "1234567790",
+    createdAt: "2023-03-13T17:34:44.317Z",
+    updatedAt: "2023-03-13T17:34:44.317Z",
+  },
+  {
+    _id: "640f135aee0733bd4a2a8d4e",
+    name: "Heba Skhail Recipient 1",
+    mobile: "+970597039224",
+    idNumber: "1234567890",
+    createdAt: "2023-03-13T12:13:14.504Z",
+    updatedAt: "2023-03-13T17:36:59.568Z",
+  },
+  {
+    _id: "640f12460e919ceb77ca4007",
+    name: "Heba Skhail",
+    mobile: "+970597039225",
+    idNumber: "1234567890",
+    createdAt: "2023-03-13T12:08:38.005Z",
+    updatedAt: "2023-03-13T12:08:38.005Z",
+  },
+];
+
+export default function Cash() {
+  const [selectedOffice, setSelectedOffice] = useState(offices[0]);
+  const [selectedRecipient, setSelectedRecipient] = useState(recipients[0]);
+  const addModalObj = useModal();
+  const editModalObj = useModal();
+
+  return (
+    <>
+      <div className="max-w-[450px] mx-auto">
+        {/* Office select */}
+        <div className="office mb-8">
+          <p className="text-lg">Office</p>
+          <SelectList
+            selected={selectedOffice}
+            setSelected={setSelectedOffice}
+            selectedOptionComp={() => <OfficeOption office={selectedOffice} />}
+          >
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {offices.map((office, idx) => {
+                const isLast = offices.length - 1 != idx;
+                return <OfficeOption office={office} withDivider={isLast} />;
+              })}
+            </Listbox.Options>
+          </SelectList>
+        </div>
+        {/* Recipient select */}
+        <div className="recipient">
+          <p className="text-lg">
+            Recipient{" "}
+            <span
+              className="text-xs text-blue-light cursor-pointer ml-2"
+              onClick={editModalObj.openModal}
+            >
+              Edit
+            </span>
+          </p>
+          <SelectList
+            selected={selectedRecipient}
+            setSelected={setSelectedRecipient}
+            selectedOptionComp={() => (
+              <RecipentOption recipient={selectedRecipient} />
+            )}
+          >
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg  focus:outline-none sm:text-sm z-50">
+              {recipients.map((recipient, idx) => {
+                const isLast = recipients.length - 1 != idx;
+                return (
+                  <RecipentOption recipient={recipient} withDivider={isLast} />
+                );
+              })}
+            </Listbox.Options>
+          </SelectList>
+          <div className="flex justify-end  ml-2 mt-2">
+            <span
+              className="flex items-center text-sm text-blue-light cursor-pointer"
+              onClick={addModalObj.openModal}
+            >
+              <PlusIcon width={"20px"} /> Add recipient
+            </span>
+          </div>
+        </div>
+      </div>
+      <Modal {...addModalObj}>
+        <TransferCard
+          centerTitle={false}
+          title="Add Recipient"
+          closeModal={addModalObj.closeModal}
+        >
+          <AddRecipient />
+        </TransferCard>
+      </Modal>
+
+      <Modal {...editModalObj}>
+        <TransferCard
+          centerTitle={false}
+          title="Recipient"
+          closeModal={editModalObj.closeModal}
+        >
+          <EditRecipient />
+        </TransferCard>
+      </Modal>
+    </>
+  );
 }
 
-export const Cash = () => {
-    const modalRecipient = useModal();
-    const modalDelete = useModal();
-    const modalAdd = useModal();
-
-    const dataOffice = [
-        {
-            address: 'غزة - مكتب الدانا',
-            addressDes: "الرمال _ تقاطع شارع فلسطين مع الشهداء",
-            dateWork: "ساعات العمل: 9 صباحا -7مساءا",
-            commission: "بدون عمولة"
-        },
-        {
-            address: 'غزة - مكتب البرعصي',
-            addressDes: "الرمال _ تقاطع شارع فلسطين مع الشهداء",
-            dateWork: "ساعات العمل: 9 صباحا -7مساءا",
-            commission: "بدون عمولة"
-        },
-        {
-            address: 'غزة - مكتب شقليه',
-            addressDes: "الرمال _ تقاطع شارع فلسطين مع الشهداء",
-            dateWork: "ساعات العمل: 9 صباحا -7مساءا",
-            commission: "بدون عمولة"
-        },
-        {
-            address: 'غزة - مكتب حرزالله',
-            addressDes: "الرمال _ تقاطع شارع فلسطين مع الشهداء",
-            dateWork: "ساعات العمل: 9 صباحا -7مساءا",
-            commission: "بدون عمولة"
-        },
-    ]
-
-    const [text, setText] = useState('');
-
-    const [recipient, setRecipient] = useState({
-        name: '',
-        phone: '',
-        id: '',
-    });
-
-    const handleRecipient = (data) => {
-        setRecipient(data);
-    }
-
-    const handleParagraphClick = (event: any) => {
-        var value: any = Math.floor(event.target.textContent);
-        setText(value);
-    }
-
-    const { register, handleSubmit, formState: { errors } } = useForm<FormValues>();
-
-    const validateNumberField = (value: string): string | boolean => {
-        if (value.includes(".")) {
-            return "You can't withdraw cents in cash.";
-        }
-        return true;
-    };
-
-    const handleKeyPress = (event: any) => {
-        if (event.key === "+" || event.key === "-") {
-            event.preventDefault();
-        }
-    }
-
-    const onSubmit = (data: FormValues) => {
-        console.log(data);
-    };
-
-    return (
-        <>
-            <div className="flex justify-center mt-6 gap-6">
-                <div>
-                    <p className="text-base text-[#707070]">Amount</p>
-                </div>
-                <div className="flex gap-2">
-                    <p className=" text-[#9E9E9E] text-xs flex items-center">Available</p>
-                    <p onClick={handleParagraphClick} className="text-[#4375FF] text-sm flex items-center cursor-pointer">255.5</p>
-                </div>
-            </div>
-            <form onSubmit={handleSubmit(onSubmit)} noValidate>
-                <div className="flex justify-center">
-                    <Input type="number" className="w-48 text-lg text-right text-black" placeholder="$00.00" min={0}
-                        {...register("amount", {
-                            required: "field is empty",
-                            validate: validateNumberField,
-                            onChange: (e) => setText(() => e.target.value),
-                        })}
-                        onKeyPress={handleKeyPress}
-                        id="amount"
-                        value={text}
-
-                    />
-                </div>
-                {errors.amount && <p className="text-[#EE404C] text-center mt-[-30px] mb-3">{errors.amount.message}</p>}
-
-                <div className="flex flex-col ml-20 mt-[-20px]">
-                    <div>
-                        <p className="text-[#707070]">Office</p>
-                    </div>
-                    <SelectListBox data={dataOffice} />
-                </div>
-
-                <div className="flex flex-col ml-20 mt-4 z-0">
-                    <div className="flex gap-2">
-                        <p className="text-[#707070]">Recipient</p>
-                        <button type="button" className="text-[#4375FF]" onClick={modalRecipient.openModal}>edit</button>
-                    </div>
-                    {/* <ListBoxRecipient /> */}
-                    <SelectListBox data={dataOffice} />
-
-                    <div className="flex justify-end w-10/12 mt-1">
-                        <button type="button" className="text-[#4375FF] flex" onClick={modalAdd.openModal}>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                            </svg>
-                            Add recipient
-                        </button>
-                    </div>
-                </div>
-
-
-                <div className="flex justify-center mt-4">
-                    <Button className="w-9/12" type="submit">Withdraw</Button>
-                </div>
-            </form>
-
-            <Modal {...modalRecipient}>
-                <TransferCard centerTitle={false} title="Recipients" closeModal={modalRecipient.closeModal}>
-                    <div className="mb-8">
-                        <MiniCard className='!py-2 mb-3 hover:border-blue'>
-                            <div onClick={() => handleRecipient({ name: "محمد خلف", phone: "Phone: 0599898987", id: "ID: 488345512" })} className="flex justify-between items-start items-start flex-row-reverse">
-                                <div className="main">
-                                    <h3 className="text-lg font-semibold text-right">محمد خلف</h3>
-                                    <p className="text-right text-gray-400 text-sm mt-[6px]">Phone: 0599898987</p>
-                                </div>
-                                <div>
-                                    <div className="flex flex-col sm:flex-row justify-start ml-[-12px]">
-                                        <IconButton onClick={modalDelete.openModal}><DeleteIcon /></IconButton>
-                                        <IconButton><EditIcon /></IconButton>
-                                    </div>
-                                    <p className="text-gray-400 text-sm">ID: 488345512</p>
-                                </div>
-                            </div>
-                        </MiniCard>
-                        <MiniCard className='!py-2 mb-3'>
-                            <div className="flex justify-between items-start">
-                                <div className="main">
-                                    <h3 className="text-lg font-semibold">Sada Mousa</h3>
-                                    <p>0452-1064559-001-3100-000</p>
-                                </div>
-
-                                <div className="flex flex-col sm:flex-row">
-                                    <IconButton><DeleteIcon /></IconButton>
-                                    <IconButton><EditIcon /></IconButton>
-                                </div>
-                            </div>
-                        </MiniCard>
-                        <MiniCard className='!py-2 mb-3'>
-                            <div className="flex justify-between">
-                                <div className="main">
-                                    <h3 className="text-lg font-semibold">Sada Mousa</h3>
-                                    <p>0452-1064559-001-3100-000</p>
-                                </div>
-
-                                <div className="flex flex-col sm:flex-row">
-                                    <IconButton><DeleteIcon /></IconButton>
-                                    <IconButton><EditIcon /></IconButton>
-                                </div>
-                            </div>
-                        </MiniCard>
-
-                    </div>
-                    <div className="flex gap-3">
-                        <Button fullWidth={true} className="!bg-transparent !text-blue-light hover:bg-blue hover:text-white border-gray-light border-2" >Add</Button>
-                        <Button fullWidth={true}>Select</Button>
-                    </div>
-                </TransferCard>
-            </Modal>
-
-            <Modal {...modalDelete} className="!w-80">
-                <TransferCard centerTitle={false} closeModal={modalDelete.closeModal}>
-                    <div className="mb-5 mt-10">
-                        <p>Are you sure you want to delete this recipient?</p>
-                    </div>
-                    <div className="flex gap-3">
-                        <Button fullWidth={true} className="bg-white hover:bg-white text-black border-gray-light border-2" >Cancel</Button>
-                        <Button fullWidth={true} className="bg-red hover:bg-red">Delete</Button>
-                    </div>
-                </TransferCard>
-            </Modal>
-
-            <ModalAddRecipient {...modalAdd} />
-        </>
-    );
+const RecipentOption = ({ recipient, withDivider }) => {
+  return (
+    <Listbox.Option
+      className={`relative cursor-pointer select-none pt-2 pl-6 pr-6 list-none`}
+      value={recipient}
+    >
+      <div className={`block truncate`}>
+        <div className="flex justify-end mb-1">
+          <span className="font-bold">{recipient.name}</span>
+        </div>
+        <div className="flex justify-between items-end text-sm text-gray-400">
+          <span>{`Phone: ${recipient.mobile}`}</span>
+          <span>{`ID: ${recipient.idNumber}`}</span>
+        </div>
+      </div>
+      {withDivider && <Divider />}
+    </Listbox.Option>
+  );
 };
-export default Cash;
+const OfficeOption = ({ office, withDivider }) => {
+  return (
+    <Listbox.Option
+      className={`relative cursor-pointer select-none pt-2 pl-6 pr-6 list-none`}
+      value={office}
+    >
+      <div className={`block truncate`}>
+        <div className="flex justify-between items-end mb-1">
+          <span className="font-bold">{office.name}</span>
+          <span className="text-xs text-gray-400">
+            ساعات العمل {office.startingHour} صباحا- {office.endingHour}مساءً
+          </span>
+        </div>
+        <div className="flex justify-between items-end text-xs text-gray-400">
+          <span>{office.address}</span>
+          <span>{office.fees == 0 ? "بدون عمولة" : office.fees}</span>
+        </div>
+      </div>
+      {withDivider && <Divider />}
+    </Listbox.Option>
+  );
+};

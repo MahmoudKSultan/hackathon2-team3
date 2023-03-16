9090c5d8e247741550380878c6180a33b0ee4e8f
MahmoudKSultan <mahmoodkns@hotmail.com>
2023-03-16 11:01:46 +0200

    mahmoud

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,4 +3,5 @@ export * from "./useDeepCompareMemoize";
 export * from "./useRoutePrefetch";
 export * from "./useRouterEvents";
 export * from "./useSwrFetch";
+export * from "./useSwrMutationFetch";
 export * from "./useModal";
